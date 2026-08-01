d870f143f3ccd44690ddd6e81ac838b075337587
Gagan kumar SV <gagankumarsv143@gmail.com>
2026-08-01 20:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (11.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,17 @@
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        @lru_cache(None)
+        def dp(left, right):
+            if left == right:
+                return nums[left]
+
+            pick_left = nums[left] - dp(left + 1, right)
+            pick_right = nums[right] - dp(left, right - 1)
+
+            return max(pick_left, pick_right)
+
+        return dp(0, len(nums) - 1) >= 0
+        
