fe8ae483ef5f716856bf3c20c52d57d871d49b40
Gagan kumar SV <gagankumarsv143@gmail.com>
2026-09-09 20:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.75%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans = 0
+        if n >= 1000:
+            end = min(n, 999999)
+            ans += end - 1000 + 1
+
+        if n >= 1000000:
+            end = min(n, 999999999)
+            ans += (end - 1000000 + 1) * 2
+
+        if n >= 1000000000:
+            end = min(n, 999999999999)
+            ans += (end - 1000000000 + 1) * 3
+
+        if n >= 1000000000000:
+            end = min(n, 999999999999999)
+            ans += (end - 1000000000000 + 1) * 4
+
+        if n >= 1000000000000000:
+            ans += (n - 1000000000000000 + 1) * 5
+
+        return ans
